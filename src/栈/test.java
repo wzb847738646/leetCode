bfe1fce609b1c422ecb1565021e3fb06c24d8e50
84773 <847738646@qq.com>
2020-08-23 09:37:38 +0800

    leetCode232 用栈实现队列

--- src/栈/test.java
+++ src/栈/test.java
@@ -4,22 +4,11 @@ public class test {
 
 
     public static void main(String[] args) {
-        leetCode_225 l1 = new leetCode_225();
+        leetCode_232 l1 = new leetCode_232();
         l1.push(1);
         l1.push(2);
-        l1.push(3);
-        l1.push(4);
-        l1.top();
+        l1.peek();
         l1.pop();
-        l1.top();
-        l1.pop();
-        l1.top();
-        l1.pop();
-        l1.top();
-        l1.pop();
-
-
-
-
+        l1.empty();
     }
 }
